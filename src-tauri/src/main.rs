09302b4fbf0db1816829ab933e95b55bc1a5a812
1084350607 <1084350607@qq.com>
2023-07-10 21:20:16 +0800

    feat: support splash screen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::Manager;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn close_splashscreen(window: tauri::Window) {
+  // 关闭启动视图
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // 展示主视图
+  window.get_window("main").unwrap().show().unwrap();
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    // 注册命令
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,4 +1,21 @@
 <template>
+  <div data-tauri-drag-region class="titlebar">
+    <div class="titlebar-button" id="titlebar-minimize">
+      <img
+        src="https://api.iconify.design/mdi:window-minimize.svg"
+        alt="minimize"
+      />
+    </div>
+    <div class="titlebar-button" id="titlebar-maximize">
+      <img
+        src="https://api.iconify.design/mdi:window-maximize.svg"
+        alt="maximize"
+      />
+    </div>
+    <div class="titlebar-button" id="titlebar-close">
+      <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+    </div>
+  </div>
   <div class="homepage">
     <div>拖动你想放置的软件1</div>
   </div>
@@ -6,7 +23,9 @@
 
 <script setup lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import { WebviewWindow } from "@tauri-apps/api/window";
+import { WebviewWindow, appWindow } from "@tauri-apps/api/window";
+import { invoke } from "@tauri-apps/api/tauri";
+import * as app from "@tauri-apps/api/app";
 
 defineComponent({
   name: "homepage",
@@ -15,19 +34,51 @@ defineComponent({
 const url = ref();
 
 onMounted(() => {
-  const webview = new WebviewWindow("theUniqueLabel", {
-    url: "https://weread.qq.com/web/reader/864325c052a341864c4cdd2",
-    transparent: true,
-  });
+  // const webview = new WebviewWindow("theUniqueLabel", {
+  //   // url: "https://weread.qq.com/web/reader/864325c052a341864c4cdd2",
+  //   transparent: true,
+  //   titleBarStyle: "transparent",
+  //   hiddenTitle: true,
+  //   minimizable: false,
+  // });
+  //
+  // webview.once("tauri://created", function () {
+  //   // webview window successfully created
+  //   console.log(123);
+  // });
 
-  webview.once("tauri://created", function () {
-    // webview window successfully created
-    console.log(123);
+  let win = appWindow;
+  win.set_transparent_titlebar(true, false);
+
+  // 添加监听函数，监听 DOM 内容加载完成事件
+  document.addEventListener("DOMContentLoaded", () => {
+    invoke("close_splashscreen");
   });
 });
 </script>
 
 <style lang="scss" scoped>
+.titlebar {
+  height: 30px;
+  background: #329ea3;
+  user-select: none;
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.titlebar-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+}
+.titlebar-button:hover {
+  background: #5bbec3;
+}
 .homepage {
   display: flex;
   justify-content: center;
